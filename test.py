fe735e1975bed2c3d059b47c65670b70e338e0ec
maint <tom.lebrat@gmail.com>
2023-02-22 23:36:14 +0100

    commit 6

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 5, 0.1)
+y = np.sin(x)
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.savefig("ffffffffff")
+#plt.show()
+
+
